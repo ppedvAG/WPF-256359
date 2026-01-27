bc1f0c05a6bdb415be4106b0a13c0ce35e382e09
ppedvLukasK <lukask@ppedv.de>
2026-01-27 15:08:54 +0100

    M004

---
+++ M004/FourValueToMarginConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace M004;
+
+class FourValueToMarginConverter : IMultiValueConverter
+{
+	public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+	{
+		//double[] zahlen = new double[values.Length];
+		//for (int i = 0; i < zahlen.Length; i++)
+		//{
+		//	zahlen[i] = (double)values[i];
+		//}
+		//Thickness t = new Thickness(zahlen[0], zahlen[1], zahlen[2], zahlen[3]);
+		//return t;
+
+		double[] z = values.Select(e => (double) e).ToArray();
+		return new Thickness(z[0], z[1], z[2], z[3]);
+	}
+
+	public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ M004/ScoreToGradeConverter.cs
@@ -0,0 +1,65 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace M004;
+
+class ScoreToGradeConverter : IValueConverter //Strg + .: Schnelloptionen anzeigen
+{
+	/// <summary>
+	/// Convert: Quelle zu Ziel
+	/// Slider -> TextBlock
+	/// Double -> String
+	/// </summary>
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		//string note;
+		//double d = (double) value;
+		//switch (d)
+		//{
+		//	case < 50:
+		//		note = "Nicht Genügend";
+		//		break;
+		//	case < 60:
+		//		note = "Mangelhaft";
+		//		break;
+		//	case < 70:
+		//		note = "Ausreichend";
+		//		break;
+		//	case < 80:
+		//		note = "Befriedigend";
+		//		break;
+		//	case < 90:
+		//		note = "Gut";
+		//		break;
+		//	case <= 100:
+		//		note = "Sehr Gut";
+		//		break;
+		//	default:
+		//		note = "Unbekannt";
+		//		break;
+		//}
+		//return note + $" ({d})";
+
+		double d = (double) value;
+		return d switch
+		{
+			< 50 => "Nicht Genügend",
+			< 60 => "Mangelhaft",
+			< 70 => "Ausreichend",
+			< 80 => "Befriedigend",
+			< 90 => "Gut",
+			<= 100 => "Sehr Gut",
+			_ => "Unbekannt",
+		} + $" ({d})";
+	}
+
+	/// <summary>
+	/// Ziel -> Quelle
+	/// String -> Double
+	/// Hier nicht notwendig, weil der TextBlock nicht verändert werden kann
+	/// </summary>
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}

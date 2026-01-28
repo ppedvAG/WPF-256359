48092baa951cf4ddf89c1218978bfef0e3ff0830
ppedvLukasK <lukask@ppedv.de>
2026-01-28 12:27:43 +0100

    M006, M007

---
+++ M006/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace M006;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		//Resources im Backend angreifen
+		Output.Text = this.Resources["DefaultBackgroundColor"].ToString();
+	}
+}
---
+++ M007/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace M007;
+
+public partial class MainWindow : Window
+{
+	public ObservableCollection<Fahrzeug> Fahrzeuge { get; set; } =
+	[
+		new Fahrzeug(251, FahrzeugMarke.BMW),
+		new Fahrzeug(274, FahrzeugMarke.BMW),
+		new Fahrzeug(146, FahrzeugMarke.BMW),
+		new Fahrzeug(208, FahrzeugMarke.Audi),
+		new Fahrzeug(189, FahrzeugMarke.Audi),
+		new Fahrzeug(133, FahrzeugMarke.VW),
+		new Fahrzeug(253, FahrzeugMarke.VW),
+		new Fahrzeug(304, FahrzeugMarke.BMW),
+		new Fahrzeug(151, FahrzeugMarke.VW),
+		new Fahrzeug(250, FahrzeugMarke.VW),
+		new Fahrzeug(217, FahrzeugMarke.Audi),
+		new Fahrzeug(125, FahrzeugMarke.Audi)
+	];
+
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}

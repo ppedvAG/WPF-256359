a5bacbe05a7ecd287ad8dfb669f4d673d81a4901
ppedvLukasK <lukask@ppedv.de>
2026-01-29 15:00:55 +0100

    M012

---
+++ M012/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace M012;
+
+public partial class MainWindow : Window
+{
+	public ObservableCollection<Person> Personen { get; set; }
+
+	public Person Selected { get; set; }
+
+	public MainWindow()
+	{
+		string readJson = File.ReadAllText(@"..\..\..\Personen.json");
+		Personen = new ObservableCollection<Person>(JsonSerializer.Deserialize<List<Person>>(readJson)!);
+
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		//1. Möglichkeit
+		//Personen.Remove(Selected);
+
+		//2. Möglichkeit
+		Button b = (Button)sender;
+		Person p = b.DataContext as Person;
+		Personen.Remove(p);
+    }
+}
+
+[DebuggerDisplay("Person - ID: {ID}, Vorname: {Vorname}, Nachname: {Nachname}, GebDat: {Geburtsdatum.ToString(\"yyyy.MM.dd\")}, Alter: {Alter}, " +
+	"Jobtitel: {Job.Titel}, Gehalt: {Job.Gehalt}, Einstellungsdatum: {Job.Einstellungsdatum.ToString(\"yyyy.MM.dd\")}")]
+public record Person(int ID, string Vorname, string Nachname, DateTime Geburtsdatum, int Alter, Beruf Job, List<string> Hobbies);
+
+public record Beruf(string Titel, int Gehalt, DateTime Einstellungsdatum);

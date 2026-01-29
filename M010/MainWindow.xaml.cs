038a3c77ad3433f4bd4c2010f6c84cf7f445739d
ppedvLukasK <lukask@ppedv.de>
2026-01-29 10:04:41 +0100

    M010

---
+++ M010/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace M010;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		TB.FontSize++;
+	}
+}
